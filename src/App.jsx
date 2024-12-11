272db26192fed867c313bc23427a71a3c2758f87
anjankumardas5 <anjantechnology507@gmail.com>
2024-12-11 18:12:50 +0530

    o

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename='/User_'>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
